f70b08b84eadb78dddea4995c8dec941814859fa
hanaTsuk1 <101488209+hanaTsuk1@users.noreply.github.com>
2023-09-17 11:44:38 +0800

    fix: random color

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -132,7 +132,14 @@ export function formatHHmmss(time: number) {
   })
 }
 
-export const randomColor = () => `#${(Math.random() * 0xFFFFFF << 0).toString(16)}`
+export function randomColor() {
+  const letters = '0123456789ABCDEF'
+  let color = '#'
+  for (let i = 0; i < 6; i++)
+    color += letters[Math.floor(Math.random() * 16)]
+
+  return color
+}
 
 export const createIconBlob = (buffer: number[]) => new Blob([new Uint8Array(buffer)], { type: 'image/png' })
 
