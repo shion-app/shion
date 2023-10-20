ae818ae503997d064ee5866bf4ed02ef7092db90
hanaTsuk1 <101488209+hanaTsuk1@users.noreply.github.com>
2023-10-20 09:50:05 +0800

    refactor(database): 类型推导

--- src/modules/database/index.ts
+++ src/modules/database/index.ts
@@ -1,6 +1,6 @@
 import Database from 'tauri-plugin-sql-api'
 import { error } from 'tauri-plugin-log-api'
-import type { Selectable } from 'kysely'
+import type { SelectType } from 'kysely'
 
 import type { DatabaseExecutor } from './db'
 import { DatabaseError, SqliteErrorEnum, createKyselyDatabaseWithModels, findSqliteMessageFields } from './db'
@@ -30,9 +30,23 @@ const executor: DatabaseExecutor = {
 
 export const db = createKyselyDatabaseWithModels(executor)
 
-export type SelectProgram = Selectable<Program>
-export type SelectActivity = Selectable<Activity>
-export type SelectPlan = Selectable<Plan>
-export type SelectLabel = Selectable<Label>
-export type SelectNote = Selectable<Note>
-export type SelectMoment = Selectable<Moment>
+type DrainOuterGeneric<T> = [T] extends [unknown] ? T : never
+type IfNotNever<T, K> = T extends never ? never : K
+
+type NonNeverSelectKeys<R> = {
+  [K in keyof R]: IfNotNever<SelectType<R[K]>, K>;
+}[keyof R]
+
+type DeepSelectable<R> = DrainOuterGeneric<{
+  [K in NonNeverSelectKeys<R>]:
+  SelectType<R[K]> extends object
+    ? DeepSelectable<SelectType<R[K]>>
+    : SelectType<R[K]>;
+}>
+
+export type SelectProgram = DeepSelectable<Program>
+export type SelectActivity = DeepSelectable<Activity>
+export type SelectPlan = DeepSelectable<Plan>
+export type SelectLabel = DeepSelectable<Label>
+export type SelectNote = DeepSelectable<Note>
+export type SelectMoment = DeepSelectable<Moment>
