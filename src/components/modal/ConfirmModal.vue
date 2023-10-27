b391d9870e1f8915bf2cb164ca05b80f6fdd560a
hanaTsuk1 <101488209+hanaTsuk1@users.noreply.github.com>
2023-10-27 21:35:31 +0800

    添加vue-final-modal

---
+++ src/components/modal/ConfirmModal.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { VueFinalModal } from 'vue-final-modal'
+
+defineProps<{
+  title: string
+  content?: string
+}>()
+
+const emit = defineEmits<{
+  (e: 'confirm'): void
+}>()
+</script>
+
+<template>
+  <VueFinalModal
+    flex justify-center items-center
+  >
+    <v-card :title="title" min-width="400" max-width="600">
+      <v-card-text>
+        {{ content }}
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer />
+        <v-btn
+          text="Close Dialog"
+          @click="emit('confirm')"
+        />
+      </v-card-actions>
+    </v-card>
+  </VueFinalModal>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import 'uno.css'
 import '@unocss/reset/tailwind.css'
+import 'vue-final-modal/style.css'
 import './styles/index.scss'
 
 import { createApp } from 'vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 import { createPinia } from 'pinia'
 import { createVuetify } from 'vuetify'
+import { createVfm } from 'vue-final-modal'
 
 import VChart from 'vue-echarts'
 import { use } from 'echarts/core'
@@ -45,6 +47,8 @@ const vuetify = createVuetify({
   },
 })
 
+const vfm = createVfm()
+
 use([
   CanvasRenderer,
   BarChart,
@@ -62,6 +66,7 @@ createApp(App)
   .use(router)
   .use(pinia)
   .use(vuetify)
+  .use(vfm)
   .component('v-chart', VChart)
   .mount('#app')
 
