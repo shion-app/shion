c0e5b7c2bfc6bb60f5ff19a488eb432676beabae
hanaTsuk1 <101488209+hanaTsuk1@users.noreply.github.com>
2023-12-13 14:57:36 +0800

    status bar使用tooltip button

--- src/components/footer/StatusBar.vue
+++ src/components/footer/StatusBar.vue
@@ -9,6 +9,6 @@ const timerText = computed(() => route.fullPath == '/timer' ? text.value : `#${t
 <template>
   <div px-4 flex>
     <div flex-1 />
-    <v-btn v-if="running" :text="timerText" variant="text" />
+    <tooltip-button v-if="running" :tooltip="$t('statusBar.timer')" location="top" :text="timerText" variant="text" />
   </div>
 </template>
