cbeb23407205df986e4dc2b00d7b2ac0fc83c651
hanaTsuk1 <101488209+hanaTsuk1@users.noreply.github.com>
2023-12-30 14:06:20 +0800

    empty延时

--- src/components/shared/Empty.vue
+++ src/components/shared/Empty.vue
@@ -4,10 +4,20 @@ import img from '@/assets/no-data.svg'
 defineProps<{
   desc?: string
 }>()
+
+const invisible = ref(true)
+
+setTimeout(() => {
+  invisible.value = false
+}, 100)
 </script>
 
 <template>
-  <div h-full flex flex-col justify-center items-center>
+  <div
+    h-full flex flex-col justify-center items-center :class="{
+      invisible,
+    }"
+  >
     <img width="200" :src="img">
     <div text-5 mt-10 text-gray-500>
       {{ $props.desc || $t('hint.empty') }}
