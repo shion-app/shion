499778d9fafa2786d4ac7af2627936d2047606e8
hanaTsuk1 <101488209+hanaTsuk1@users.noreply.github.com>
2024-01-21 22:45:06 +0800

    日历hover显示当天详情

--- src/components/widget/ActiveStatusCalendar.vue
+++ src/components/widget/ActiveStatusCalendar.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import type { EChartsOption } from 'echarts'
-import { addDays, getDay, isBefore, isSameYear } from 'date-fns'
+import { addDays, getDay, isBefore, isSameDay, isSameYear } from 'date-fns'
 
 import { type SelectActivity, type SelectNote, db } from '@/modules/database'
 
@@ -16,10 +16,26 @@ const activityList = ref<Array<SelectActivity>>([])
 const day = 51 * 7 + getDay(new Date())
 const range = generateRange(day)
 
-const list = computed(() => {
-  const array = [...splitByDay(noteList.value, range), ...splitByDay(activityList.value, range)]
+const list = computed(() => [
+  ...splitByDay(noteList.value, range).map(i => ({
+    start: i.start,
+    end: i.end,
+    name: i.label.name,
+    color: i.label.color,
+    type: 'note',
+  })),
+  ...splitByDay(activityList.value, range).map(i => ({
+    start: i.start,
+    end: i.end,
+    name: i.program.name,
+    color: i.program.color,
+    type: 'activity',
+  })),
+])
+
+const calendarList = computed(() => {
   const map = new Map<string, number>()
-  for (const { start, end } of array) {
+  for (const { start, end } of list.value) {
     const date = format(start, 'yyyy-MM-dd')
     map.set(date, (map.get(date) || 0) + end - start)
   }
@@ -32,8 +48,10 @@ const list = computed(() => {
   return [...map.entries()]
 })
 
-const min = computed(() => Math.min(...list.value.map(([_, value]) => value)))
-const max = computed(() => Math.max(...list.value.map(([_, value]) => value)))
+const min = computed(() => Math.min(...calendarList.value.map(([_, value]) => value)))
+const max = computed(() => Math.max(...calendarList.value.map(([_, value]) => value)))
+
+const buildMarker = color => `<span style="display:inline-block;margin-right:4px;border-radius:10px;width:10px;height:10px;background-color:${color};"></span>`
 
 const option = computed<EChartsOption>(() => {
   return {
@@ -73,13 +91,33 @@ const option = computed<EChartsOption>(() => {
         const [time, value] = params.value
         const date = new Date(time)
         const dateText = isSameYear(new Date(), date) ? format(date, 'MM-dd') : format(date, 'yyyy-MM-dd')
-        return `<div style="margin-bottom: 6px;">${dateText}</div><div>${params.marker} <span style="float:right;margin-left:20px;font-size:14px;color:#666;font-weight:900">${formatHHmmss(value)}</span></div>`
+        const detail = list.value.filter(i => isSameDay(i.start, date))
+        const colorMap = new Map<string, string>()
+        const timeMap = new Map<string, number>()
+        for (const { name, start, end, type, color } of detail) {
+          const key = `${type}|${name}`
+          timeMap.set(key, (timeMap.get(key) || 0) + end - start)
+          colorMap.set(key, color)
+        }
+        const displayList = [...timeMap.entries()]
+          .map(([key, value]) => [key, value] as [string, number])
+          .sort((a, b) => b[1] - a[1])
+          .map(([key, value]) => [key.split('|').pop(), value, colorMap.get(key)] as [string, number, string])
+        const template = displayList.map(([name, time, color]) => `<div style="display: flex; align-items: center;">${buildMarker(color)}<span style="margin-left: 6px;">${name}</span><div style="min-width: 50px; flex-grow: 1;"></div>${formatHHmmss(time)}<span></span></div>`).join('')
+        return `<div style="margin-bottom: 6px;">
+                  <span>${dateText}</span>
+                  <span style="float:right;margin-left:20px;font-size:14px;color:#666;font-weight:900">${formatHHmmss(value)}</span>
+                </div>
+                <div>
+                  ${template}
+                </div>
+                `
       },
     },
     series: {
       type: 'heatmap',
       coordinateSystem: 'calendar',
-      data: list.value,
+      data: calendarList.value,
     },
   }
 })
