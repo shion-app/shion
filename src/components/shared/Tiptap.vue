43ee1897f656b8a3d4c3fcae30821f9984269c36
hanaTsuk1 <101488209+hanaTsuk1@users.noreply.github.com>
2024-01-29 16:42:43 +0800

    tiptap 工具栏分隔、状态

--- src/components/shared/Tiptap.vue
+++ src/components/shared/Tiptap.vue
@@ -20,6 +20,19 @@ interface DialogFilter {
   extensions: string[]
 }
 
+interface Divider {
+  type: 'divider'
+}
+
+interface Button {
+  icon: string
+  tip: string
+  handler: () => Promise<void>
+  isActive?: () => boolean
+}
+
+type Util = Divider | Button
+
 const props = defineProps<{
   content: string
   editable: boolean
@@ -110,29 +123,34 @@ async function uploadVideo() {
 
 const utils = computed(() => [
   {
-    icon: 'i-mdi:format-quote-close',
-    tip: t('moment.editor.quote'),
-    handler: call(c => c.toggleBlockquote()),
-  },
-  {
     icon: 'i-mdi:format-bold',
     tip: t('moment.editor.bold'),
     handler: call(c => c.toggleBold()),
+    isActive: isActive('bold'),
   },
   {
     icon: 'i-mdi:format-italic',
     tip: t('moment.editor.italic'),
     handler: call(c => c.toggleItalic()),
+    isActive: isActive('italic'),
   },
   {
     icon: 'i-mdi:format-underline',
     tip: t('moment.editor.underline'),
     handler: call(c => c.toggleUnderline()),
+    isActive: isActive('underline'),
   },
   {
     icon: 'i-mdi:format-strikethrough',
     tip: t('moment.editor.strikethrough'),
     handler: call(c => c.toggleStrike()),
+    isActive: isActive('strike'),
+  },
+  {
+    icon: 'i-mdi:format-quote-close',
+    tip: t('moment.editor.quote'),
+    handler: call(c => c.toggleBlockquote()),
+    isActive: isActive('blockquote'),
   },
   // {
   //   icon: 'i-mdi:code-tags',
@@ -143,6 +161,7 @@ const utils = computed(() => [
     icon: 'i-mdi:xml',
     tip: t('moment.editor.codeBlock'),
     handler: call(c => c.toggleCodeBlock()),
+    isActive: isActive('codeBlock'),
   },
   // {
   //   icon: 'i-mdi:link-variant',
@@ -150,19 +169,28 @@ const utils = computed(() => [
   //   handler: call(c => c.toggleLink()),
   // },
   {
+    type: 'divider',
+  },
+  {
     icon: 'i-mdi:format-list-bulleted',
     tip: t('moment.editor.listBulleted'),
     handler: call(c => c.toggleBulletList()),
+    isActive: isActive('bulletList'),
   },
   {
     icon: 'i-mdi:format-list-numbered',
     tip: t('moment.editor.listNumbered'),
     handler: call(c => c.toggleOrderedList()),
+    isActive: isActive('orderedList'),
   },
   {
     icon: 'i-mdi:checkbox-marked-outline',
     tip: t('moment.editor.taskList'),
     handler: call(c => c.toggleTaskList()),
+    isActive: isActive('taskList'),
+  },
+  {
+    type: 'divider',
   },
   {
     icon: 'i-mdi:image',
@@ -174,7 +202,11 @@ const utils = computed(() => [
     tip: t('moment.editor.video'),
     handler: uploadVideo,
   },
-])
+] as Array<Util>)
+
+function isDivider(util: Util): util is Divider {
+  return (util as Divider).type == 'divider'
+}
 
 function call(command: (chain: ChainedCommands) => ChainedCommands) {
   if (!editor.value)
@@ -184,6 +216,10 @@ function call(command: (chain: ChainedCommands) => ChainedCommands) {
   return () => command(chain).run()
 }
 
+function isActive(name: string) {
+  return () => editor.value?.isActive(name) || false
+}
+
 watchOnce(contentVModel, (v) => {
   editor.value?.commands.setContent(v, false)
 })
@@ -191,16 +227,23 @@ watchOnce(contentVModel, (v) => {
 
 <template>
   <div v-if="$props.editable" flex>
-    <tooltip-button
-      v-for="{ icon, tip, handler } in utils"
-      :key="icon"
-      :tooltip="tip"
-      location="bottom"
-      :icon="icon"
-      size="small"
-      variant="text"
-      @click="handler"
-    />
+    <template v-for="item, index in utils">
+      <template v-if="isDivider(item)">
+        <v-divider :key="item.type + index" vertical class="mx-2!" />
+      </template>
+      <template v-else>
+        <tooltip-button
+          :key="item.icon"
+          :tooltip="item.tip"
+          location="bottom"
+          :icon="item.icon"
+          size="small"
+          variant="text"
+          :active="item.isActive?.()"
+          @click="item.handler"
+        />
+      </template>
+    </template>
   </div>
   <v-divider my-2 />
   <EditorContent :editor="editor" overflow-y-auto :class="contentClass" />
