f113a9bc5b557a5318f7bb9c1bf2c3c212a1e437
hanaTsuk1 <101488209+hanaTsuk1@users.noreply.github.com>
2024-04-22 10:03:44 +0800

    history 导入进度

--- src/stores/useHistoryStore.ts
+++ src/stores/useHistoryStore.ts
@@ -1,6 +1,9 @@
 import { getConfig, readHistory, setConfig } from 'tauri-plugin-shion-history-api'
 
+import type { InsertHistory } from '@/modules/database'
 import { db } from '@/modules/database'
+
+type History = Omit<InsertHistory, 'domainId'>
 
 export const useHistoryStore = defineStore('history', () => {
   const { state } = useAsyncState(getConfig(), {
@@ -9,24 +12,70 @@ export const useHistoryStore = defineStore('history', () => {
     shallow: false,
   })
 
-  async function pull(list: string[], start: number, end: number) {
-    const historyList = await readHistory(list, start, end)
-    await db.history.batchInsert(historyList.map(({ title, url, last_visited }) => ({
-      title,
-      url,
-      lastVisited: last_visited,
-    })))
+  const completedCount = ref(0)
+  const totalCount = ref(0)
+  const requesting = ref(false)
+  const progress = computed(() => `${~~(completedCount.value * 100 / totalCount.value)}%`)
+
+  const { listen } = useDatabase()
+
+  listen('history.insert', () => {
+    completedCount.value++
+  })
+
+  async function insert(browserList: string[], historyList: Array<History>, end: number) {
+    await db.history.batchInsert(historyList)
     for (const browser of state.value.browsers) {
-      if (list.includes(browser.name))
+      if (browserList.includes(browser.name))
         browser.last_sync = end
     }
     await setConfig(state.value)
   }
 
+  async function getHistory(list: string[], start: number, end: number) {
+    return (await readHistory(list, start, end)).map(({ title, url, last_visited }) => ({
+      title,
+      url,
+      lastVisited: last_visited,
+    }))
+  }
+
+  async function pull(browserList: string[], start: number, end: number) {
+    completedCount.value = 0
+    totalCount.value = 0
+    requesting.value = true
+    try {
+      const historyList = await getHistory(browserList, start, end)
+      totalCount.value += historyList.length
+      await insert(browserList, historyList, end)
+    }
+    finally {
+      requesting.value = false
+    }
+  }
+
   async function pullActiveBrowsers() {
-    for (const browser of state.value.browsers) {
-      if (browser.last_sync > 0)
-        await pull([browser.name], browser.last_sync, new Date().getTime())
+    completedCount.value = 0
+    totalCount.value = 0
+    requesting.value = true
+    try {
+      const map = new Map<string, Array<History>>()
+      for (const browser of state.value.browsers) {
+        if (browser.last_sync > 0) {
+          const historyList = await getHistory([browser.name], browser.last_sync, new Date().getTime())
+          map.set(browser.name, historyList)
+          totalCount.value += historyList.length
+        }
+      }
+      for (const browser of state.value.browsers) {
+        if (browser.last_sync > 0) {
+          const historyList = map.get(browser.name)!
+          await insert([browser.name], historyList, new Date().getTime())
+        }
+      }
+    }
+    finally {
+      requesting.value = false
     }
   }
 
@@ -34,5 +83,7 @@ export const useHistoryStore = defineStore('history', () => {
     config: state,
     pull,
     pullActiveBrowsers,
+    progress,
+    requesting,
   }
 })
