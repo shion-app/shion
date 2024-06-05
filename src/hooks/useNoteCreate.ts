c02837c1892bb925c00e4c522357ac6f7532a8cf
hanaTsuk1 <101488209+hanaTsuk1@users.noreply.github.com>
2024-06-05 13:06:15 +0800

    手动记录添加直接录入数据

--- src/hooks/useNoteCreate.ts
+++ src/hooks/useNoteCreate.ts
@@ -1,12 +1,13 @@
 import { db } from '@/modules/database'
 import type { InsertNote, SelectLabel, SelectPlan } from '@/modules/database'
 
-type NoteForm = Pick<InsertNote, 'planId' | 'labelId'>
+type NoteForm = Pick<InsertNote, 'planId' | 'labelId'> & Partial<Pick<InsertNote, 'start' | 'end'>> & { direct?: boolean }
 
 export function useNoteCreate() {
   const { t } = useI18n()
   const { parseFieldsError } = useDatabase()
-  const { start, setText } = useTimerStore()
+  const { start: startTimer, setText } = useTimerStore()
+  const { success } = useNotify()
 
   const { open, close, setModelValue } = useFormModal<NoteForm, {
     planList: Array<SelectPlan>
@@ -48,12 +49,50 @@ export function useNoteCreate() {
                   autoSelectFirst: 'exact',
                 },
               },
+              {
+                type: 'checkbox',
+                key: 'direct',
+                label: t('note.fill.direct'),
+                props: {
+                  'onUpdate:modelValue': () => {
+                    setModelValue({
+                      start: new Date().getTime(),
+                      end: new Date().getTime(),
+                    })
+                  },
+                },
+              },
+              {
+                type: 'datetimePicker',
+                key: 'start',
+                label: t('note.fill.start'),
+                visible: !!model.direct,
+                props: {
+                  max: model.end,
+                },
+              },
+              {
+                type: 'datetimePicker',
+                key: 'end',
+                label: t('note.fill.end'),
+                visible: !!model.direct,
+                props: {
+                  min: model.start,
+                },
+              },
             ],
           },
-          schema: z => z.object({
-            planId: z.number(),
-            labelId: z.number(),
-          }),
+          schema: (z) => {
+            const start = z.number()
+            const end = z.number()
+            return z.object({
+              planId: z.number(),
+              labelId: z.number(),
+              direct: z.boolean().optional(),
+              start: model.direct ? start : start.optional(),
+              end: model.direct ? end : end.optional(),
+            })
+          },
           async onConfirm(v, setErrors) {
             setText(labelList.find(i => i.id == v.labelId)!.name)
             let noteId = 0
@@ -65,7 +104,11 @@ export function useNoteCreate() {
               return setErrors(parseFieldsError(error))
             }
             close()
-            start(() => db.note.update(noteId, {
+            if (v.direct) {
+              success({})
+              return
+            }
+            startTimer(() => db.note.update(noteId, {
               end: Date.now(),
             }))
           },
@@ -81,10 +124,12 @@ export function useNoteCreate() {
 
   function handleCreate(note: NoteForm) {
     const now = Date.now()
+    const { planId, labelId, direct, start, end } = note
     return db.note.insert({
-      ...note,
-      start: now,
-      end: now,
+      planId,
+      labelId,
+      start: direct ? start! : now,
+      end: direct ? end! : now,
     })
   }
 
