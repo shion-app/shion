05e8a5c73737574547d2d6bc6e97b4bf6bad3354
hanaTsuk1 <101488209+hanaTsuk1@users.noreply.github.com>
2024-06-19 13:26:23 +0800

    修改changelog显示实现

--- src/components/dialog/ChangelogDialog.vue
+++ src/components/dialog/ChangelogDialog.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import { MdPreview } from 'md-editor-v3'
 
-import type { Release } from '@/modules/changelog'
-import { parseChangelog } from '@/modules/changelog'
+import zhCN from '../../../changelog/zh-CN.md?raw'
+import enUS from '../../../changelog/en-US.md?raw'
+import { type Release, parseChangelog } from '@/modules/changelog'
 
 const props = defineProps<{
   visible: boolean
@@ -11,7 +12,16 @@ const props = defineProps<{
 const { visible: visibleVModel } = useVModels(props)
 const configStore = useConfigStore()
 
-const { config, ready } = storeToRefs(configStore)
+const { config } = storeToRefs(configStore)
+
+const data = computed(() => {
+  switch (config.value.locale) {
+    case 'zh-CN':
+      return zhCN
+    default:
+      return enUS
+  }
+})
 
 const changelog = ref<Release>({
   notes: '',
@@ -21,25 +31,11 @@ const changelog = ref<Release>({
 
 const content = computed(() => `## ${changelog.value.title}\n${changelog.value.notes}`)
 
-function b64DecodeUnicode(str: string) {
-  return decodeURIComponent(Array.prototype.map.call(atob(str), (c) => {
-    return `%${(`00${c.charCodeAt(0).toString(16)}`).slice(-2)}`
-  }).join(''))
-}
-
-async function init() {
-  const url = `https://api.github.com/repos/shion-app/shion/contents/changelog/${config.value.locale}.md`
-  const data = await (await fetch(url)).json()
-  if (data.content) {
-    const release = await parseChangelog(b64DecodeUnicode(data.content), config.value.version)
-    if (release)
-      changelog.value = release
-  }
-}
-
-watch(ready, init)
-
-watch(() => config.value.locale, init)
+watch(data, async (v) => {
+  const release = await parseChangelog(v, config.value.version)
+  if (release)
+    changelog.value = release
+})
 </script>
 
 <template>
