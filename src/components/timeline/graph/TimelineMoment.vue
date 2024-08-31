b8bd8e2e489c320bd4ec17a74ec55dabc3bc38c6
hanaTsuk1 <101488209+hanaTsuk1@users.noreply.github.com>
2024-08-31 09:15:22 +0800

    timeline moment去除跳转默认行为

--- src/components/timeline/graph/TimelineMoment.vue
+++ src/components/timeline/graph/TimelineMoment.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { open } from '@tauri-apps/plugin-shell'
+
 import type { ObsidianNote } from '@/hooks/useObsidian'
 import type { TimeLineNodeCommonGraphData } from '@/interfaces'
 
@@ -10,16 +12,24 @@ const props = defineProps<{
 defineOptions({
   inheritAttrs: false,
 })
+
+function openObsidian() {
+  open(`obsidian://open?path=${props.raw.path}`)
+}
 </script>
 
 <template>
-  <a v-bind="$attrs" :href="`obsidian://open?path=${props.raw.path}`">
-    <div i-mdi:pencil />
+  <div v-bind="$attrs" i-mdi:pencil cursor-pointer @click="openObsidian">
     <v-tooltip activator="parent" :text="$t('timelineGraph.moment.tooltip')" location="start" />
-  </a>
+  </div>
   <div>
-    <div class="line-clamp-1" break-all>
+    <div :class="props.data.children ? 'line-clamp-2' : 'line-clamp-1'" break-all>
       {{ props.data.title }}
+    </div>
+    <div v-if="props.data.children">
+      {{ $t('timelineGraph.includeCount', {
+        count: props.data.children,
+      }) }}
     </div>
   </div>
 </template>
