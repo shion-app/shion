81e1882fea438c5df66155a33434add31730ec54
hanaTsuk1 <101488209+hanaTsuk1@users.noreply.github.com>
2024-09-07 10:34:26 +0800

    remark paginationSelect

--- src/modules/database/models/remark.ts
+++ src/modules/database/models/remark.ts
@@ -1,4 +1,4 @@
-import { type Kysely } from 'kysely'
+import { type Kysely, sql } from 'kysely'
 import { jsonBuildObject } from 'kysely/helpers/sqlite'
 
 import type { DB, Remark as TransformRemark } from '../transform-types'
@@ -21,7 +21,7 @@ export class Remark extends Model<TransformRemark> {
   }
 
   @get()
-  select(value?: { start?: number; end?: number; programId?: number }) {
+  select(value?: { start?: number; end?: number; programId?: number; keyword?: string; page?: number; size?: number }) {
     let query = this.kysely.with('p', () => this.#program.select()).selectFrom(['remark', 'p']).where('remark.deletedAt', '=', 0)
     if (value?.start)
       query = query.where('time', '>', value.start)
@@ -29,6 +29,14 @@ export class Remark extends Model<TransformRemark> {
       query = query.where('time', '<', value.end)
     if (value?.programId)
       query = query.where('programId', '=', value.programId)
+    if (value?.keyword) {
+      query = query.where(eb => eb.or([
+        eb('remark.title', 'like', `%${value.keyword}%`),
+        eb('remark.desc', 'like', `%${value.keyword}%`),
+      ])).orderBy('remark.time desc')
+    }
+    if (value?.page && value?.size)
+      query = query.offset((value.page - 1) * value.size).limit(value.size)
 
     return query.select(eb =>
       jsonBuildObject({
@@ -44,5 +52,30 @@ export class Remark extends Model<TransformRemark> {
         totalTime: eb.ref('p.totalTime'),
       }).as('program'),
     ).selectAll(this.table).whereRef('remark.programId', '=', 'p.id')
+  }
+
+  @get(false)
+  count(value?: { keyword?: string }) {
+    let query = this.selectByLooseType()
+    if (value?.keyword) {
+      query = query.where(eb => eb.or([
+        eb('remark.title', 'like', `%${value.keyword}%`),
+        eb('remark.desc', 'like', `%${value.keyword}%`),
+      ]))
+    }
+    return query.select(sql<number>`ifnull(count(*), 0)`.as('count'))
+  }
+
+  paginationSelect(value: { keyword?: string; page: number; size: number }) {
+    const { page, size } = value
+    return this.transaction().execute(async (trx) => {
+      const [{ count }] = await trx.remark.count(value)
+      const list = await trx.remark.select(value)
+      return {
+        list,
+        next: count > page * size,
+        count,
+      }
+    })
   }
 }
